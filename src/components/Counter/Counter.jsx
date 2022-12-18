a974c5882d15c2e7e4178b1f09d300c7e506df1e
Maryna-Korbet <m22v86k@gmail.com>
2022-12-18 08:17:00 +0200

    registered events and states

--- src/components/Counter/Counter.jsx
+++ src/components/Counter/Counter.jsx
@@ -2,13 +2,35 @@ import css from 'components/Counter/Counter.module.css';
 import { Component } from 'react';
 
 class Counter extends Component {
+
+    static defaultProps = {
+        initialValue: 0,
+    };
+
+    state = {
+        value: this.props.initialValue,
+    };
+
+    handleIncrement = () => {
+        this.setState(prevState => ({
+            value: prevState.value + 1,
+        }));
+    };
+
+    handleDecrement = () => {
+        this.setState(prevState => ({
+            value: prevState.value - 1,
+        }));
+    };
+
+
     render() {
         return (
             <div className={css.counter}>
-                <div className={css.value}>0</div>
+                <span className={css.value}>{this.state.value}</span>
                 <div>
-                    <button type="button" className={css.button}>Increment by 1</button>
-                    <button type="button" className={css.button}>Decrement by 1</button>
+                    <button type="button" className={css.button} onClick={this.handleIncrement}>Increment by 1</button>
+                    <button type="button" className={css.button} onClick={this.handleDecrement}>Decrement by 1</button>
                 </div>
             </div>
     )  
